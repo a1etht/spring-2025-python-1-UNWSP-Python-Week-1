20b5884c2cad2f12a2bde6769cdbdb2556cc8238
Alethea Lo <aylo@students.unwsp.edu>
2025-01-21 16:24:20 -0600

    Update program_2.py
    Fixed the spacing with script.

--- program_2.py
+++ program_2.py
@@ -7,6 +7,5 @@ def proverbs_22_29():
     print('Do you see a man skillful in his work?'
           'He will stand before kings;'
           'he will not stand before obscure men.')
-    
 
 proverbs_22_29()
