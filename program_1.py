72bb6902ec7c6ede1c6d651df95fe61fad429c79
Alethea Lo <aylo@students.unwsp.edu>
2025-01-21 16:15:01 -0600

    Modified program 1
    Add my name, date, and notes to Hello World script.

--- program_1.py
+++ program_1.py
@@ -1,3 +1,7 @@
+#Programmer: Alethea Lo
+#Date: 1/21/25
+
+#Display text
 def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
--- program_2.py
+++ program_2.py
@@ -1,6 +1,12 @@
+#Programmer: Alethea Lo
+#Date: 1/21/25
+
+#Display text:
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
-    print('...')
+    print('Do you see a man skillful in his work?'
+          'He will stand before kings;'
+          'he will not stand before obscure men.')
     
 
 proverbs_22_29()
