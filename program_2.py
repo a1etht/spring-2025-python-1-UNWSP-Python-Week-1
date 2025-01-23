b18e92049426e445039442d2fecce9b3dcf6ada1
Alethea Lo <aylo@students.unwsp.edu>
2025-01-22 22:22:48 -0600

    Add files via upload

--- program_2.py
+++ program_2.py
@@ -1,11 +1,8 @@
 #Programmer: Alethea Lo
-#Date: 1/21/25
+#Date: 1/22/25
 
-#Display text:
-def proverbs_22_29():
-    # Write your Proverbs 22:29 code below
-    print('Do you see a man skillful in his work?'
-          'He will stand before kings;'
-          'he will not stand before obscure men.')
-
-proverbs_22_29()
+#Display text
+print('“Do you see a man skillful in his work? '
+      'He will stand before kings; '
+      'he will not stand before obscure men.” '
+      '(Proverbs 22:29)')
